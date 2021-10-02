b1c1bcfc10a18ae65bb3c2675e8f75dcba1f9785
maxim <maximdubovoi@gmail.com>
2021-10-02 10:43:26 +0300

    use pillow

--- bitmap/3_bitmap_message.py
+++ bitmap/3_bitmap_message.py
@@ -1,4 +1,35 @@
 import sys
+# check Pillow version number
+import PIL
+
+print('Pillow Version:', PIL.__version__)
+
+# load and show an image with Pillow
+from PIL import Image
+from numpy import asarray
+
+# Open the image form working directory
+image = Image.open('bitmap/Max.jpg')
+# summarize some details about the image
+print(image.format)
+print(image.size)
+print(image.mode)
+
+# convert image to numpy array
+data = asarray(image)
+print(type(data))
+# summarize shape
+print(data.shape)
+
+# create Pillow image
+image2 = Image.fromarray(data)
+print(type(image2))
+
+# summarize image details
+print(image2.mode)
+print(image2.size)
+
+print(data)
 
 bitmap = """
 ....................................................................
@@ -34,5 +65,5 @@ for line in bitmap.splitlines():
         if bit == ' ':
             print(' ', end='')
         else:
-            print(message[i%len(message)], end='')
+            print(message[i % len(message)], end='')
     print()
